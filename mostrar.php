83f1986ec3dc63a6c9b5a724ec7e55521cb1ed8f
nicolas <nicolas@NICO>
2017-01-24 20:15:46 -0300

    mostrar.php y registra.php:Se estilizan las paginas para mejor apariencia
    register.html:No se realizan cambios

--- mostrar.php
+++ mostrar.php
@@ -12,49 +12,72 @@ if(!$_SESSION["validate"]){
 ?>
 
 <html>
-<head>
-	<title>Mostrar tabla</title>
-</head>
-<body>
-	<table border="1" cellspacing=1 cellpadding=2 style="font-size: 8pt"><tr>
-	<td><font face="verdana"><b>Nombre</b></font></td>
-	<td><font face="verdana"><b>Apellido</b></font></td>
-	<td><font face="verdana"><b>Direccion</b></font></td>
-	<td><font face="verdana"><b>Telefono</b></font></td>
-	<td><font face="verdana"><b>Mail</b></font></td>
-	<td><font face="verdana"><b>Id Persona</b></font></td>
-	</tr>
+    <head>
+	    <title>Mostrar tabla</title>
+	    <meta charset="utf-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+	    <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+	    <script src="./bootstrap/jQuery/jquery.min.js"></script>
+	    <script src="./bootstrap/js/bootstrap.min.js"></script>
+	    <!--	
+	    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	    -->
+    </head>
+    <body>
+	<div class="container">
+	    <div title="Titulo" class="text-center text-info">
+		<h1>
+		    Tabla Nombres
+		</h1>		
+	    </div>
 
-	<?php  
-          include("abre_conexion.php");  
-        
-	  $query = "SELECT nombre,apellido,direccion,telefono,mail,idpersona " .
-	      "FROM Persona";
-	  $result = $conexion_db->query($query);
-	  $numero = 0;
-	  while($row = $result->fetch_assoc())
-	  {
-	    echo "<tr><td width=\"25%\"><font face=\"verdana\">" . 
-		    $row["nombre"] . "</font></td>";
-	    echo "<td width=\"25%\"><font face=\"verdana\">" . 
-		    $row["apellido"] . "</font></td>";
-	    echo "<td width=\"25%\"><font face=\"verdana\">" . 
-		    $row["direccion"] . "</font></td>";
-	    echo "<td width=\"25%\"><font face=\"verdana\">" . 
-		    $row["telefono"]. "</font></td>";   
-	    echo "<td width=\"25%\"><font face=\"verdana\">" . 
-		    $row["mail"]. "</font></td>";
-	    echo "<td width=\"25%\"><font face=\"verdana\">" . 
-		    $row["idpersona"]. "</font></td></tr>";
-	    $numero++;
-	  }
-	  echo "<tr><td colspan=\"15\"><font face=\"verdana\"><b>Número: " . $numero . 
-	      "</b></font></td></tr>";
-	  
-          $result->free();
-          include("cierra_conexion.php");  
-          echo "<p>Volver a las opciones anteriores.</p>  <p><a href='javascript:history.go(-1)'>VOLVER ATRÁS</a></p>";  
-	?>
-	</table>
-</body>
+	    <table class="table table-bordered table-hover table-striped">
+		<thead>
+		<tr>
+		    <th><font face="verdana"><b>Nombre</b></font></th>
+		    <th><font face="verdana"><b>Apellido</b></font></th>
+		    <th><font face="verdana"><b>Direccion</b></font></th>
+		    <th><font face="verdana"><b>Telefono</b></font></th>
+		    <th><font face="verdana"><b>Mail</b></font></th>
+		    <th><font face="verdana"><b>Id Persona</b></font></th>
+		</tr>
+		</thead>
+		<tbody>
+		<?php  
+		    include("abre_conexion.php");  
+
+		    $query = "SELECT nombre,apellido,direccion,telefono,mail,idpersona " .
+			"FROM Persona";
+		    $result = $conexion_db->query($query);
+		    $numero = 0;
+		    while($row = $result->fetch_assoc())
+		    {
+			echo "<tr><td width=\"25%\"><font face=\"verdana\">" . 
+				$row["nombre"] . "</font></td>";
+			echo "<td width=\"25%\"><font face=\"verdana\">" . 
+				$row["apellido"] . "</font></td>";
+			echo "<td width=\"25%\"><font face=\"verdana\">" . 
+				$row["direccion"] . "</font></td>";
+			echo "<td width=\"25%\"><font face=\"verdana\">" . 
+				$row["telefono"]. "</font></td>";   
+			echo "<td width=\"25%\"><font face=\"verdana\">" . 
+				$row["mail"]. "</font></td>";
+			echo "<td width=\"25%\"><font face=\"verdana\">" . 
+				$row["idpersona"]. "</font></td></tr>";
+			$numero++;
+		    }
+		    echo "<tr><td colspan=\"15\"><font face=\"verdana\"><b>Número: " . $numero . 
+			"</b></font></td></tr>";
+
+		    $result->free();
+		    include("cierra_conexion.php");  
+		    //echo "<p>Volver a las opciones anteriores.</p>  <p><a href='javascript:history.go(-1)'>VOLVER ATRÁS</a></p>";  
+		?>
+		</tbody>
+	    </table>
+	    <div title="Boton regresar" class="text-center">
+		<a href="inicio.php" class="btn btn-success"> Volver</a>
+	    </div>
+	</div>
+    </body>
 </html>
--- registra.php
+++ registra.php
@@ -14,18 +14,50 @@ if(!$_SESSION["validate"]){
 
     <head>  
 	<title>Carga de datos</title>  
+	    <meta charset="utf-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+	    <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+	    <script src="./bootstrap/jQuery/jquery.min.js"></script>
+	    <script src="./bootstrap/js/bootstrap.min.js"></script>
+	    <!--	
+	    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	    -->	
     </head>  
 
-    <body>  
-	<form method="POST" action="registra.php">
-	    <p>Nombre    <input type="text" name="nombre" size="20"></p>
-	    <p>Apellido  <input type="text" name="apellido" size="20"></p>
-	    <p>Direccion <input type="text" name="direccion" size="20"></p>
-	    <p>Telefono  <input type="text" name="telefono" size="20"></p>
-	    <p>Mail      <input type="text" name="mail" size="20"></p>  
-
-	    <p><input type="submit" value="Guardar datos" name="B1"></p>  
-	</form>
+    <body>
+	<div class="container center-block">
+	    <div class="col-md-3"></div>
+	<div class="col-md-6">
+	    <div class="form-area">  
+		<form role="form" class="form-horizontal" method="POST">
+		<br style="clear:both">
+			    <h3 style="margin-bottom: 25px; text-align: center;">Contact Form</h3>
+					<div class="form-group">
+						<input type="text" class="form-control" id="nombre" name="nombre" placeholder="Nombre" required>
+					</div>
+					<div class="form-group">
+						<input type="text" class="form-control" id="apellido" name="apellido" placeholder="Apellido" required>
+					</div>
+					<div class="form-group">
+						<input type="text" class="form-control" id="telefono" name="telefono" placeholder="Número" required>
+					</div>
+					<div class="form-group">
+						<input type="text" class="form-control" id="mail" name="mail" placeholder="Email" required>
+					</div>
+					<div class="form-group">
+						<input type="text" class="form-control" id="direccion" name="direccion" placeholder="Direccion" required>
+					</div>
+		    <div title="Boton guardar" class="text-center">
+			<input type="submit" value="Guardar datos" name="B1" class="btn btn-success">
+		    </div>
+		</form>
+	    </div>
+	</div>
+	<div class="col-md-3"></div> 
+	</div>
+	<div title="Boton regresar" class="text-left">
+		<a href="inicio.php" class="btn btn-default">Volver</a>
+	</div>
 	<?php  
 	    
 	    $nombre = filter_input(INPUT_POST, "nombre");
@@ -34,7 +66,7 @@ if(!$_SESSION["validate"]){
 	    $telefono = filter_input(INPUT_POST,"telefono");
 	    $mail = filter_input(INPUT_POST, "mail");
 	    
-	    if(isset($nombre)){
+	    if(isset($nombre) && isset($apellido) && isset($direccion) && isset($telefono) && isset($mail)){
 		require_once("abre_conexion.php");  
 
 		$_GRABAR_SQL = "INSERT INTO Persona (nombre,apellido,direccion,telefono,mail) VALUES ('$nombre','$apellido','$direccion','$telefono','$mail')";
@@ -43,12 +75,12 @@ if(!$_SESSION["validate"]){
 		require_once("cierra_conexion.php"); 
 	    }
  
-
-	    echo "  
-	    <p>Volver.</p>  
-
-	    <p><a href='inicio.php'>ATRÁS</a></p>";  
-	?>  
+//
+//	    echo "  
+//	    <p>Volver.</p>  
+//
+//	    <p><a href='inicio.php'>ATRÁS</a></p>";  
+	?> 
     </body>  
 
 </html>   
